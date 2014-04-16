64d5a9cbe5de9f2a7771d16adb905506db6cd3a4
jdehlin <jdehlin@gmail.com>
2014-04-15 22:42:42 -0400

    Better namespacing referencing

--- SimpleSample/RootViewController.cs
+++ SimpleSample/RootViewController.cs
@@ -2,13 +2,14 @@
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using XamarinSidebar;
 
 namespace Sample
 {
 	public partial class RootViewController : UIViewController
 	{
 		// the sidebar controller for the app
-		public XamarinSidebar.SidebarController SidebarController { get; private set; }
+		public SidebarController SidebarController { get; private set; }
 
 		public RootViewController() : base(null, null)
 		{
@@ -20,7 +21,7 @@ namespace Sample
 			base.ViewDidLoad();
 
 			// create a slideout navigation controller with the top navigation controller and the menu view controller
-			SidebarController = new XamarinSidebar.SidebarController(this, new IntroController(), new SideMenuController());
+			SidebarController = new SidebarController(this, new IntroController(), new SideMenuController());
 		}
 	}
 }
